1249a3ce63347a832b44f8c58eea62a61faa6d03
branyoe <bhernandez1@ucol.mx>
2022-10-21 14:21:08 -0500

    Fix: changes in bathroomView

--- src/views/BathroomView/index.jsx
+++ src/views/BathroomView/index.jsx
@@ -1,7 +1,7 @@
-import { Button, Divider, Grid, TextField, Typography } from "@mui/material";
+import { Avatar, Button, Divider, Grid, Rating, TextField, Typography } from "@mui/material";
 import { Box, Stack } from "@mui/system";
-import { useEffect, useState, useCallback } from "react";
-// import { useAuth } from "../../context/authContext";
+import { useEffect, useState, useCallback, useContext } from "react";
+import CloseIcon from '@mui/icons-material/Close';
 import { addComment, watchComments } from "../../DB";
 import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
@@ -10,11 +10,13 @@ import { useFormik } from "formik";
 import Comments from "./components/Comments";
 import { Loading } from "../../components";
 import { useAuth } from "../../context/authContext"
+import { MapContext } from "../../context";
 
-export default function BathroomView({bathroom, setOpen}) {
+export default function BathroomView({ bathroom, setOpen }) {
   const [comments, setComments] = useState([]);
   const { user } = useAuth();
-  
+  const {map} = useContext(MapContext);
+
 
   const queryComments = useCallback(async () => {
     await watchComments(bathroom.id, setComments);
@@ -24,23 +26,6 @@ export default function BathroomView({bathroom, setOpen}) {
     queryComments()
   }, [queryComments])
 
-  const validationSchema = yup.object({
-    calf: yup
-      .number('solo numeros')
-      .required('campo requerido')
-      .min(0, "valores del 0 al 5")
-      .max(5, "valores del 0 al 5")
-  });
-
-  const formik = useFormik({
-    initialValues: {
-      calf: 0
-    },
-    validationSchema: validationSchema,
-    onSubmit: (values) => {
-
-    },
-  });
 
   const commentVlidationSchema = yup.object({
     comment: yup
@@ -65,67 +50,92 @@ export default function BathroomView({bathroom, setOpen}) {
 
   return (
     <>
-      <Box sx={{ bgcolor: "#eee", height: "100vh", width: "100vw" }}>
-        <Box sx={{ padding: 1, paddingTop: 1 }}>
-          <Stack mb={3}>
+      <Box sx={{ height: "100vh", width: "100vw" }}>
+        <CloseIcon
+          style={{ position: "fixed", top: 10, left: "90%" }}
+          onClick={() => setOpen()}
+        />
+        <Box sx={{ padding: 1, paddingTop: 4 }}>
+          <Stack mb={5}>
             <Grid container spacing={1}>
               <Grid maxHeight="85px" item xs={4}>
-                <Box onClick={() => {
-                  setOpen()
-                }} sx={{ height: "100%", bgcolor: "white", p: 1, borderRadius: 3 }}>
-                  <Typography
-                    variant="subtitle2"
-                    color="text.primary"
-                    fontWeight={600}
-                    align="center"
-                  >
-                    Calificación
-                  </Typography>
-                  <Typography
-                    variant="h4"
-                    align="center"
-                  >
-                    4
-                  </Typography>
+                <Box sx={{ height: "100%", bgcolor: "white", p: 1, borderRadius: 3 }}>
+                  <Avatar sx={{ height: 100, width: 100 }} src="/static/images/avatar/2.jpg" >B</Avatar>
                 </Box>
               </Grid>
               <Grid maxHeight="85px" item xs={8}>
-                <Box overflow="scroll" sx={{ height: "100%", bgcolor: "white", p: 1, borderRadius: 3 }}>
-
+                <Box overflow="scroll" sx={{ maxHeight: "80px", bgcolor: "", p: 1 }}>
                   <Typography
                     variant="h6"
-                    color="text.primary"
-                    fontWeight={500}
+                    fontWeight={600}
                   >
                     {bathroom.name}
                   </Typography>
                 </Box>
+                <Stack>
+                  <Stack direction="row" alignItems="center">
+                    <AttachMoneyIcon fontSize="large" />
+                    <Typography variant="h6" >{bathroom.type}</Typography>
+                  </Stack>
+                </Stack>
+              </Grid>
+              <Grid item xs={12} mt={8}>
+                {/* <Stack
+                bgcolor="white"
+                borderRadius={3}
+                mb={2}
+                p={1}
+              >
+                <Stack>
+                  <Stack
+                    component="form"
+                    onSubmit={formik.handleSubmit}
+                    noValidate sx={{ mt: 0 }}
+                    direction="column"
+                    justifyContent="center"
+                    alignItems="center"
+                  >
+                    <TextField
+                      id="standard-number"
+                      label="Califíca este baño"
+                      type="number"
+                      variant="standard"
+                      name="calf"
+                      value={formik.values.calf}
+                      onChange={formik.handleChange}
+                      error={formik.touched.calf && Boolean(formik.errors.calf)}
+                      helperText={formik.touched.calf && formik.errors.calf}
+                    />
+                    <Button type="submit">Enviar</Button>
+                  </Stack>
+                </Stack>
+              </Stack> */}
+                <Stack justifyContent="center" alignItems="center">
+                  <Typography mb={2} variant="h4" component="legend">5</Typography>
+                  <Rating
+                    name="simple-controlled"
+                    value={5}
+                    onChange={(event, newValue) => {
+                      // setValue(newValue);
+                    }}
+                  />
+                  <Typography mt={1} component="legend">Califica este baño</Typography>
+                </Stack>
               </Grid>
             </Grid>
           </Stack>
           <Stack
             bgcolor="white"
-            mb={2}
-            borderRadius={3}
-            p={1}
-          >
-            <Stack>
-              <Typography
-                variant="subtitle2"
-              >
-                Tipo
-              </Typography>
-              <Stack direction="row" alignItems="center">
-                <AttachMoneyIcon fontSize="large" />
-                <Typography variant="h5" >{bathroom.type}</Typography>
-              </Stack>
-            </Stack>
-          </Stack>
-          <Stack
-            bgcolor="white"
             borderRadius={3}
             mb={2}
             p={1}
+            onClick={() => {
+              setOpen();
+              map?.flyTo({
+                zoom: 14,
+                center: [bathroom.lng, bathroom.lat]
+              })
+            }}
           >
             <Stack>
               <Typography
@@ -135,37 +145,7 @@ export default function BathroomView({bathroom, setOpen}) {
               </Typography>
               <Stack direction="row" alignItems="center">
                 <LocationOnIcon fontSize="large" />
-                <Typography variant="subtitle2" >{bathroom.lat}, {bathroom.lng}</Typography>
-              </Stack>
-            </Stack>
-          </Stack>
-          <Stack
-            bgcolor="white"
-            borderRadius={3}
-            mb={2}
-            p={1}
-          >
-            <Stack>
-              <Stack
-                component="form"
-                onSubmit={formik.handleSubmit}
-                noValidate sx={{ mt: 0 }}
-                direction="column"
-                justifyContent="center"
-                alignItems="center"
-              >
-                <TextField
-                  id="standard-number"
-                  label="Califíca este baño"
-                  type="number"
-                  variant="standard"
-                  name="calf"
-                  value={formik.values.calf}
-                  onChange={formik.handleChange}
-                  error={formik.touched.calf && Boolean(formik.errors.calf)}
-                  helperText={formik.touched.calf && formik.errors.calf}
-                />
-                <Button type="submit">Enviar</Button>
+                <Typography variant="subtitle2" >{bathroom.address}</Typography>
               </Stack>
             </Stack>
           </Stack>
